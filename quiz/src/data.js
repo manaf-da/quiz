af7541919b1ff4dabcedee3db0da19523b7a54ac
manaf-da <57951139+manaf-da@users.noreply.github.com>
2023-06-18 22:29:59 +0200

    Add files via upload
    fist part of quiz app

---
+++ quiz/src/data.js
@@ -0,0 +1,84 @@
+export const quiz = {
+  questions: [
+    {
+      question: "What is the capital of Australia?",
+      options: ["Sydney", "Melbourne", "Canberra", "Perth"],
+      correctAnswer: "canberra",
+      selectedAnswer: "",
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      options: ["Mars", "Jupiter", "Venus", "Saturn"],
+      correctAnswer: "Mars",
+      selectedAnswer: "",
+    },
+    {
+      question: "Who wrote the play 'Romeo and Juliet'?",
+      options: [
+        "William Shakespeare",
+        "Jane Austen",
+        "Mark Twain",
+        "Charles Dickens",
+      ],
+      correctAnswer: "William Shakespeare",
+      selectedAnswer: "",
+    },
+    {
+      question: "What is the largest ocean in the world?",
+      options: [
+        "Pacific Ocean",
+        "Atlantic Ocean",
+        "Indian Ocean",
+        "Arctic Ocean",
+      ],
+      correctAnswer: "Pacific Ocean",
+      selectedAnswer: "",
+    },
+    {
+      question: "Which country is famous for the Great Wall of China?",
+      options: ["Japan", "China", "India", "South Korea"],
+      correctAnswer: "China",
+      selectedAnswer: "",
+    },
+    {
+      question: "Who is the author of the Harry Potter book series?",
+      options: [
+        "J.K. Rowling",
+        "Stephen King",
+        "George R.R. Martin",
+        "Dan Brown",
+      ],
+      correctAnswer: "J.K.Rowling",
+      selectedAnswer: "",
+    },
+    {
+      question: "What is the chemical symbol for gold?",
+      options: ["Au", "Ag", "Fe", "Cu"],
+      correctAnswer: "Au",
+      selectedAnswer: "",
+    },
+    {
+      question: "Which country won the FIFA World Cup in 2018?",
+      options: ["Germany", "Brazil", "France", "Spain"],
+      correctAnswer: "France",
+      selectedAnswer: "",
+    },
+    {
+      question: "Who painted the Mona Lisa?",
+      options: [
+        "Leonardo da Vinci",
+        "Pablo Picasso",
+        "Vincent van Gogh",
+        "Michelangelo",
+      ],
+      correctAnswer: "Leonardo da Vinci",
+      selectedAnswer: "",
+    },
+    {
+      question: "Which year was the first iPhone released?",
+      options: ["2007", "2005", "2008", "2006"],
+      correctAnswer: "2007",
+      selectedAnswer: "",
+    },
+  ],
+};
